7e01e2524b1cf89575db7054f224355b03de1c89
Laurent Broudoux <laurent.broudoux@gmail.com>
2020-04-15 09:06:02 +0200

    Adding proxy to backend service in production mode

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,21 @@
 var express = require('express')
+var proxy = require('express-http-proxy');
 var app = express()
 
-app.set('port', (process.env.PORT || 8080))
+app.set('port', (process.env.PORT || 8081))
 app.use(express.static(__dirname + '/dist'))
+
+var backend_service = process.env.BACKEND_SERVICE || undefined
+if (backend_service == undefined) {
+  console.error("CONFIG ERROR: Can't find backend service config! \nDefine `BACKEND_SERVICE` env variable.")
+} else {
+  console.info("Backend service defined at `" + backend_service + "`. Proxying all request to /api/*")
+  app.use('/api', proxy(backend_service, {
+    proxyReqPathResolver: function (req) {
+      return '/api' + req.url
+    }})
+  );
+}
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
