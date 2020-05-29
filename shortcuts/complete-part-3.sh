894dac1d81090ab3a08b207fd24fe82fbc5f5170
Laurent Broudoux <laurent.broudoux@gmail.com>
2020-05-29 12:08:51 +0200

    Fixing the shortcut for part 3

--- shortcuts/complete-part-3.sh
+++ shortcuts/complete-part-3.sh
@@ -9,13 +9,13 @@ oc new-project fruits-catalog-prod
 oc adm policy add-role-to-user edit system:serviceaccount:fruits-catalog-dev:jenkins -n fruits-catalog-prod
 oc adm policy add-role-to-group system:image-puller system:serviceaccounts:fruits-catalog-prod -n fruits-catalog-dev
 
-# Deploy the MongoDB database component.
-oc new-app mongodb-persistent --name=mongodb -p DATABASE_SERVICE_NAME=mongodb -p MONGODB_DATABASE=sampledb -l app=fruits-catalog -n fruits-catalog-prod
-
-# Deploy all the components manifests.
-cd ..
-oc create -f manifests -n fruits-catalog-prod
-
 # Deploy a Jenkins instance and pipelines for components.
+cd ..
 oc new-app jenkins-persistent -l app=jenkins -p MEMORY_LIMIT=1Gi -n fruits-catalog-dev
-oc create -f pipelines/frontend-pipeline.yml -n fruits-catalog-dev
+oc create -f pipelines/fruits-cicd-pipeline.yml -n fruits-catalog-dev
+
+# Deploy all the components template and apply it manifests.
+oc create -f manifests/template-prod.yml
+oc process fruits-catalog-prod -n fruits-catalog-prod | oc create -f - -n fruits-catalog-prod
+
+oc start-build fruits-cicd-pipeline
